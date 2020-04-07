1cb70af436aada0fabd4c7d51c899c1c4e9273c3
Cristian Prieto <me@cprieto.com>
2020-04-07 20:08:24 +0200

    [Enh] Moved all sorting to the same module

--- py/sorting.py
+++ py/sorting.py
@@ -1,5 +1,18 @@
 from typing import List, TypeVar
+
 T = TypeVar('T')
+
+def bubble_sort(elems: List[T]) -> List[T]:
+    """Sorts a list using the bubble sort algorithm"""
+    if not elems:
+        return elems
+    
+    for mx in range(len(elems), 0, -1):
+        for idx in range(1, mx):
+            if elems[idx - 1] > elems[idx]:
+                elems[idx - 1], elems[idx] = elems[idx], elems[idx - 1]
+
+    return elems
 
 def selection_sort(elems: List[T]) -> List[T]:
     """Sorts a list using the selection sort algorithm"""
@@ -14,3 +27,5 @@ def selection_sort(elems: List[T]) -> List[T]:
                 smidx = j
         elems[i], elems[smidx] = elems[smidx], elems[i]
     return elems
+
+__all__ = ['bubble_sort', 'selection_sort']
--- py/test_sorting.py
+++ py/test_sorting.py
@@ -1,4 +1,4 @@
-from bubble import bubble_sort
+from sorting import *
 
 def test_bubble_sort_is_none_when_none():
     assert bubble_sort(None) == None
@@ -12,3 +12,12 @@ def test_bubble_sort_unsorted_list_is_sorted():
 
 def test_bubble_sort_empty_list_is_empty():
     assert bubble_sort([]) == []
+
+def test_selection_sort_unsorted_returns_sorted():
+    assert selection_sort([5, 1, 4, 2, 8]) == [1, 2, 4, 5, 8]
+
+def test_selection_sort_empty_returns_empty():
+    assert selection_sort([]) == []
+
+def test_selection_sort_none_returns_none():
+    assert selection_sort(None) == None
